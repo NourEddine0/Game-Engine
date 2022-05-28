fd521aa9c2362d1681e0d734caf80be856238da5
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2022-05-28 16:16:20 +0200

    XO-game FIXED

--- src/main/scala/XO.scala
+++ src/main/scala/XO.scala
@@ -1,30 +1,22 @@
-import scala.io.StdIn.readLine
-import scala.util.matching.Regex
-import java.util.Scanner
+
 
 
 object XO {
-	def getDrawer: Array[Array[String]] => Unit = XODrawer;
-	def getController:(Array[Array[String]], String, Boolean) => Boolean = XOController;
-	def getBoard():Array[Array[String]] ={
-		var Board = Array.ofDim[String](3, 3);
-		def resetBoard(): Unit = {
-			for (i <- 0 until Board.length) {
-				for (j <- 0 until Board.length) {
-					Board(i)(j) = " ";
-				}
-			}
-		}
-		resetBoard()
-		return Board;
-	} ; //The Board
+	def getDrawer: Array[Array[String]] => Unit = XODrawer
+	def getController:(Array[Array[String]], String, Boolean) => Boolean = XOController
+	def getBoard:Array[Array[String]] = {
+		Array(Array(" ", " ", " "),
+					Array(" ", " ", " "),
+					Array(" ", " ", " "))
+	}
+
 	def XODrawer(board: Array[Array[String]]): Unit = {
 		/* Prints the whole board with its content in a square format. */
 		def printBoard(b: Array[Array[String]]): Unit = {
 		println("    a   b   c")
-		for(i <- 0 until b.length){
+		for(i <- b.indices){
 			print(s"${b.length - i} | ")
-			for(j<- 0 until b.length){
+			for(j<- b.indices){
 			print(s"${b(i)(j)} | ")
 			}
 			println(s"${b.length - i}")
@@ -34,36 +26,53 @@ object XO {
 		printBoard (board)
 	}
 
-  	def XOController(board: Array[Array[String]], input:String, turn:Boolean): Boolean = {
-			/* Resets the board to be empty for the next game. */
-
-			val pattern = "([1-3])([a-cA-C])".r     /* Input Regex pattern */
-			var index1 = 0;             /* The parsed user input. */
-			var index2 = 0;
-			/* Detects the matching of the input & returns a boolean value according
-      to the matching result. */
-			def matchInput(in: String): Boolean = in match {
-				case pattern(_*) => {
-					index1 = '3' - in(0);
-					index2 = in(1).toLower - 'a';
-					true
-				}
-				case _ => false
-			}			/* Reading Input Loop:
-				if (the input does not match || the slot ia already filled),
-				then reEnter the input. */
-
-			if (!(matchInput(input))
-				|| board(index1)(index2).toString.matches("Console.RED+\"X\"+Console.RESET|Console.GREEN+\"O\"+Console.RESET")) {
-				/* Performing the action. */
-				println("Invalid input")
-				false;
-
-
-			}
-			else{
-				board(index1)(index2) = if (turn) Console.RED+"X"+Console.RESET else Console.GREEN+"O"+Console.RESET
+	def XOController(board: Array[Array[String]], input:String, turn:Boolean): Boolean = {
+		val pattern = "([1-3])([a-c|A-C])".r     /* Input Regex pattern */
+		val XO_pattern = "(\\u001B\\[\\d+m)+(X)(\\u001B\\[\\d+m)+|(\\u001B\\[\\d+m)+(O)(\\u001B\\[\\d+m)+"
+		var index1 = 0;             /* The parsed user input. */
+		var index2 = 0
+		/* Detects the matching of the input & returns a boolean value according
+		to the matching result. */
+		def matchInput(in: String): Boolean = in match {
+			case pattern(_*) =>
+				index1 = '3' - in(0)
+				index2 = in(1).toLower - 'a'
 				true
+			case _ => false
+		}
+
+		//Resets the board if it's full
+		def resetBoardIfFull(): Unit = {
+			var counter = 0
+			for (i <- board.indices) {
+				for (j <- board.indices) {
+					if(board(i)(j) != " ") counter += 1
+				}
+			}
+			if(counter == 9){
+				println(">>>>>>> Cleaning board....")
+				println(">>>>>>> Board cleaned!")
+				for (i <- board.indices) {
+					for (j <- board.indices) {
+						board(i)(j) = " "
+					}
+				}
 			}
 		}
+
+		/**
+		 * The Controller Flow
+		 */
+		resetBoardIfFull()
+		/* if (the input does not match || the slot is not empty),
+		then it's invalid input. */
+		if (!matchInput(input)
+			|| board(index1)(index2).matches(XO_pattern)) {
+			false
+		}
+		else{ /* Performing the action. */
+			board(index1)(index2) = if (turn) Console.RED+"X"+Console.RESET else Console.GREEN+"O"+Console.RESET
+			true
+		}
+	}
 }
